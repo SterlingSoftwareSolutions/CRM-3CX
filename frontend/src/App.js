8fed10a1c2fe2a81e979e4e162f04f115d87d324
Sajith3992 <113223741+Sajith3992@users.noreply.github.com>
2022-12-20 14:03:10 +0530

    customepagePopUp (#6)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
 import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
-import './Components/LoginPage/index.css';
-import Index from './Components/LoginPage/Index';
-import Password from './Components/Pages/Password';
+
+
+import PopUp from './Components/PopCustomer/PopUp';
+
 
 
 function App() {
   return (
      <Router>
-        <div className="App">
-          <div className="auth-wrapper">
-            <div className="auth-inner">
+       
               <Routes>
-                <Route path="/sign-in" element={<Index/>} />
-                <Route path="/password" element={<Password/>}/>
+                
+                <Route path="/customer/:customer_number" element={<PopUp/>}/>
               </Routes>
-            </div>
-          </div>
-        </div>
+            
    </Router>
   
   );
