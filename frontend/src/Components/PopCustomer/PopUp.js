77e328c47e0296c11b2f9df8f6e67b2288fbdb67
Sajith3992 <113223741+Sajith3992@users.noreply.github.com>
2022-12-22 15:48:35 +0530

    customer-page-adding-url -phone-number (#8)
    * customer-page-adding-url -phone-number
    
    * create post method
    
    * uodate post method

--- frontend/src/Components/PopCustomer/PopUp.js
+++ frontend/src/Components/PopCustomer/PopUp.js
@@ -1,77 +1,172 @@
-import React, { useEffect , useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { Form, Modal, ModalHeader } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
-function PopUp() {
-  useEffect(()=>{
+const PopUp = () => {
+  useEffect(() => {
     handleShow(true);
-    }, [])
+  }, []);
+
+  //popup the page in this section
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const api = "http://127.0.0.1:8000/api/customer/"; //api url
+  const [url, setUrl] = useState("None");
+
+  //Get the data from api
+  const fetchData = (page) => {
+    try {
+      fetch(api + page)
+        .then((response) => response.json())
+        .then((customerdata) => setData(customerdata))
+        .catch((err) => {
+          console.log(err.message);
+        });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  //if call url has number remove the other sting and featch only number
+  useEffect(() => {
+    var url = window.location.href;
+    var page = url.substring(url.lastIndexOf("=") + 1);
+    setUrl(page);
+    fetchData(page);
+  });
+
+  const [data, setData] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    location: "",
+    comment: "",
+  });
+
+  const onChangeValue = (key, value) => {
+    setData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  //post method
+  let handleSubmit = async (e) => {
+    data.phone =url;
+    console.log(data);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+    fetch(api, requestOptions)
+      .then((response) => response.json())
+    handleClose();
+  };
+
+
   return (
     <div>
-      <Modal onHide={handleClose} show={show} toggle={() => setShow(!show)}>
-        <ModalHeader toggle={() => setShow(!show)}>
+      <Modal onHide={handleClose} show={show}>
+        <ModalHeader>
+          {/* page header title */}
           <Modal.Title>Customer Add</Modal.Title>
         </ModalHeader>
 
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="PopUpForm.ControlInput1">
+            {/* Genarate link addres phone text line */}
+            <Form.Group className="mb-3">
               <Form.Label>Phone Number</Form.Label>
-              <Form.Control type="text" placeholder="phone number" autoFocus />
+              <Form.Control
+                onChange={(e) => onChangeValue("phone", e.target.value)}
+                id="phone"
+                value={url}
+                type="text"
+                placeholder="phone number"
+              />
             </Form.Group>
-
-            <Form.Group className="mb-3" controlId="PopUpForm.ControlInput1">
+            {/* customer name text line */}
+            <Form.Group className="mb-3">
               <Form.Label>Customer Name</Form.Label>
-              <Form.Control type="text" placeholder="Customer Name" autoFocus />
+              <Form.Control
+                onChange={(e) => onChangeValue("name", e.target.value)}
+                id="name"
+                defaultValue={data.name}
+                type="text"
+                placeholder="Customer Name"
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="PopUpForm.ControlInput1">
+            {/* customer address text line */}
+            <Form.Group className="mb-3">
               <Form.Label>Customer Address</Form.Label>
               <Form.Control
+                onChange={(e) => onChangeValue("CustomerAddress", e.target.value)}
+                id="CustomerAddress"
+                defaultValue={data.name}
                 type="text"
                 placeholder="Customer Address"
-                autoFocus
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="PopUpForm.ControlInput1">
+            {/* Location text line */}
+            <Form.Group className="mb-3">
               <Form.Label>Location</Form.Label>
-              <Form.Control type="text" placeholder="Location" autoFocus />
+              <Form.Control
+                onChange={(e) => onChangeValue("location", e.target.value)}
+                id="Location"
+                defaultValue={data.location}
+                type="text"
+                placeholder="Location"
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="PopUpForm.ControlInput1">
+            {/* email text line */}
+            <Form.Group className="mb-3">
               <Form.Label>Email Address</Form.Label>
               <Form.Control
+                onChange={(e) => onChangeValue("email", e.target.value)}
+                id="email"
+                defaultValue={data.email}
                 type="email"
                 placeholder="name@gmail.com"
-                autoFocus
               />
             </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1">
+            {/* comment text line */}
+            <Form.Group className="mb-3">
               <Form.Label>Comment</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control
+                onChange={(e) => onChangeValue("comment", e.target.value)}
+                id="comment"
+                defaultValue={data.comment}
+                as="textarea"
+                rows={3}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          {/* close button */}
+          <Button
+            className="btn btn mt-3"
+            style={{ backgroundColor: "#16c5d5", color: "white" }}
+            onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          {/* Next button */}
+          <Button
+            className="btn btn mt-3"
+            style={{ backgroundColor: "#16c5d5", color: "white" }}
+            onClick={(e) => handleSubmit(e)}>
             Next
           </Button>
         </Modal.Footer>
       </Modal>
       <button
         className="btn btn mt-3"
-        style={{ backgroundColor: "#0c3629", color: "white" }}
+        style={{ backgroundColor: "#16c5d5", color: "white" }}
         onClick={() => handleShow(true)}>
         PopUp
       </button>
     </div>
   );
-}
+};
 
 export default PopUp;
