084aae8ec0d853765da72b411d03c0a05d490958
Malisha Ponweera <62000985+Malish96@users.noreply.github.com>
2022-12-23 11:08:16 +0530

    Changes in Add Customer Form (#13)
    * Changes in Add Customer Form
    
    * Changes in customer form
    
    Co-authored-by: Malish96 <malisha.ponweera@towcha.com.au>
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import './App.css'
-import React from 'react';
-import Config from './Components/SideBar/Config';
-import LoginPage from './Pages/LoginPage/index';
 
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import CustomerPopUp from "./Components/PopCustomer/CustomerPopUp";
 
 function App() {
   return (
     <div className="App">
-      <Config/>
+      {/* <LoginPage/> */}
+      <CustomerPopUp />
     </div>
   );
 }
