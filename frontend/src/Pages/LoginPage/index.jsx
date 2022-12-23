6e52c52f18bb0a27afaf464f62bb6c97395f0383
Malish96 <malisha.ponweera@towcha.com.au>
2022-12-23 09:02:55 +0530

    Improvement on Login

--- frontend/src/Pages/LoginPage/index.jsx
+++ frontend/src/Pages/LoginPage/index.jsx
@@ -1,63 +1,42 @@
-import React from 'react'
-import { useState } from 'react'
-import { FaRegEyeSlash } from 'react-icons/fa';
-import './index.css'
-
-const eye = <FaRegEyeSlash icon ={FaRegEyeSlash}/>
+import React from "react";
+import "./index.css";
+import { Card, Form, Input, Checkbox } from "antd";
 
 const LoginPage = () => {
-
-  const [passwordShown, setPasswordShown] = useState(false);
-  const togglePassword =() =>{
-      setPasswordShown(!passwordShown);
-  };
-
   return (
-    <div>
-       <form>
-          <h3> Login </h3>
+    <Card
+      title="Login"
+      headStyle={{ fontSize: "24px" }}
+      className="title-style"
+      style={{ boxShadow: "0 0 5px rgba(0,0,0,1" }}
+    >
+      <Form>
+        <Form.Item
+          className="input-style"
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-          <div className="mb-3"> 
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-            />
-          </div>
-        
-          <div className="mb-3">
-            <label>Password</label>
-            <input
-              type={passwordShown ? "text":"password"}
-              className="form-control"
-              placeholder="Enter password"
-            />
-            <i onClick={togglePassword}>{eye}</i>{" "}
-          </div>
-          
-          <div className="mb-3">
-            <div className="custom-control custom-checkbox">
-              <input
-                type="checkbox"
-                className="custom-control-input"
-                id="customCheck1"
-              />
-              <label className="custom-control-label" htmlFor="customCheck1">
-                Remember me
-              </label>
-            </div>
-          </div>
+        <Form.Item
+          className="input-style"
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-
-       </form>
-    </div>
-  )
-}
+        <div className="d-grid">
+          <button type="submit" className="button-style">
+            Login
+          </button>
+        </div>
+      </Form>
+    </Card>
+  );
+};
 
 export default LoginPage;
