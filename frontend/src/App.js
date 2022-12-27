b51b1035acbc50f3a53f7d15b33c0e64ae553e25
Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
2022-12-27 08:57:34 +0530

    side bar design done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import CustomerPopUp from "./Components/PopCustomer/CustomerPopUp";
+import SideNavBar from "./Components/SideNavBar/SideNavBar";
 
 function App() {
   return (
     <div className="App">
       {/* <LoginPage/> */}
-      <CustomerPopUp />
+      {/* <CustomerPopUp /> */}
+      <SideNavBar />
     </div>
   );
 }
