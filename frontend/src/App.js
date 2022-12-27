e3754ebaf347ffabd0e93fcceecbd74989a67f7f
Malish96 <malisha.ponweera@towcha.com.au>
2022-12-27 12:00:29 +0530

    dashboard orientation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,25 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import CustomerPopUp from "./Components/PopCustomer/CustomerPopUp";
 import Dashboard from "./Pages/Dashboard/index";
 import SideNavBar from "./Components/SideNavBar/SideNavBar";
+import { Col, Row } from "antd";
 
 function App() {
   return (
-    <>
+    <Row>
       <BrowserRouter>
-        <SideNavBar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="agents" element={<CustomerPopUp />} />
-          <Route path="contact" element={<Dashboard />} />
-          <Route path="*" element={<Dashboard />} />
-        </Routes>
+        <Col span={4}>
+          <SideNavBar />
+        </Col>
+        <Col span={20}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="agents" element={<CustomerPopUp />} />
+            <Route path="contact" element={<Dashboard />} />
+            <Route path="*" element={<Dashboard />} />
+          </Routes>
+        </Col>
       </BrowserRouter>
-    </>
+    </Row>
   );
 }
 
