5d47231fed81ffd0b20b2f1384f243ca858b7de1
Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
2022-12-27 14:57:28 +0530

    complete

--- frontend/src/Components/SideNavBar/SideNavBar.js
+++ frontend/src/Components/SideNavBar/SideNavBar.js
@@ -3,31 +3,32 @@ import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import "./SideNavBar.css";
+import { Dashboard } from "@mui/icons-material";
 
 const SideNavBar = () => {
   const [isExpanded, setExpandedState] = useState(false);
-  const menuItems = [
-    {
-      text: "Dashboard",
-      icon: "icon/grid.svg",
-      href: "/",
-    },
-    {
-      text: "Inquire",
-      icon: "icon/grid.svg",
-      href: "/inquire",
-    },
-    {
-      text: "Users",
-      icon: "icon/grid.svg",
-      href: "/users",
-    },
-    {
-      text: "Agents",
-      icon: "icon/grid.svg",
-      href: "/agents",
-    },
-  ];
+  // const menuItems = [
+  //   {
+  //     text: "Dashboard",
+  //     icon: "icon/grid.svg",
+  //     href: "/",
+  //   },
+  //   {
+  //     text: "Inquire",
+  //     icon: "icon/grid.svg",
+  //     href: "/inquire",
+  //   },
+  //   {
+  //     text: "Users",
+  //     icon: "icon/grid.svg",
+  //     href: "/users",
+  //   },
+  //   {
+  //     text: "Agents",
+  //     icon: "icon/grid.svg",
+  //     href: "/agents",
+  //   },
+  // ];
 
   return (
     <div
@@ -41,7 +42,6 @@ const SideNavBar = () => {
         <div className="nav-heading">
           {isExpanded && (
             <div className="nav-brand">
-              <img src="icon/" alt="nav brand" />
               <h2>Sterling BPO CRM</h2>
             </div>
           )}
@@ -56,13 +56,58 @@ const SideNavBar = () => {
             <span></span>
           </button>
         </div>
+        <div className="nav-menu"></div>
         <div className="nav-menu">
-          {menuItems.map(({text, icon ,href}) => (
-            <a href={href} className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}>
-              <img src={icon} alt="img" srcSet="" />
-              {isExpanded && <p>{text}</p>}
-            </a>
-          ))}
+          <img
+            src={require("./icons/dashboard.png")}
+            className="nav-menu-img"
+            alt="img"
+          />
+          <a
+            href={"/"}
+            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+          >
+            {isExpanded && <p>{"Dashboard"}</p>}
+          </a>
+        </div>
+        <div className="nav-menu">
+          <img
+            src={require("./icons//inquiry.png")}
+            className="nav-menu-img"
+            alt="img"
+          />
+          <a
+            href={"/inquire"}
+            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+          >
+            {isExpanded && <p>{"Inquire"}</p>}
+          </a>
+        </div>
+        <div className="nav-menu">
+          <img
+            src={require("./icons/users.png")}
+            className="nav-menu-img"
+            alt="img"
+          />
+          <a
+            href={"/users"}
+            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+          >
+            {isExpanded && <p>{"Users"}</p>}
+          </a>
+        </div>
+        <div className="nav-menu">
+          <img
+            src={require("./icons/agent.png")}
+            className="nav-menu-img"
+            alt="img"
+          />
+          <a
+            href={"/agents"}
+            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+          >
+            {isExpanded && <p>{"Agents"}</p>}
+          </a>
         </div>
       </div>
     </div>
