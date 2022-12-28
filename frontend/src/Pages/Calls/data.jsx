0e120cf3b49368f586aed97280d7b02f7bcb8ab1
Ayesh <himanthaayesh@gmail.com>
2022-12-28 13:46:52 +0530

    search calls

---
+++ frontend/src/Pages/Calls/data.jsx
@@ -0,0 +1,11 @@
+export const data = [
+{
+    phone: '0123456789',
+    customer_name: 'John Watt',
+    agent_name: 'Luke',
+    call_type: 'inquiry',
+    date: '	10/31/2022',
+    time: '4:00 AM',
+    duration: '1.32',
+    location: 'Colombo'
+}]
--- frontend/src/Pages/Calls/index.jsx
+++ frontend/src/Pages/Calls/index.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { useState } from 'react'
+import { data } from './data'
 
 const Calls = () => {
+  const [search, setSearch] = useState('');
+  console.log(search)
   return (
-    <div>Calls</div>
+    <div>
+      <input type="search" 
+      onChange={(e) => setSearch(e.target.value)}
+      placeholder= 'Search'
+       />
+      <table>
+        <thead>
+          <tr>
+            <th>Customer Phone Number</th>
+            <th>Customer Name</th>
+            <th>Agent Name</th>
+            <th>Call Type</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Call Duration</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.filter((item)=>{
+            return search.toLowerCase() === ''
+            ? item:
+            item.customer_name.toLowerCase().includes(search)|| 
+            item.agent_name.toLowerCase().includes(search)|| 
+            item.location.toLowerCase().includes(search)
+          }).map((item)=>(
+            <tr>
+              <td>{item.phone}</td>
+              <td>{item.customer_name}</td>
+              <td>{item.agent_name}</td>
+              <td>{item.call_type}</td>
+              <td>{item.date}</td>
+              <td>{item.time}</td>
+              <td>{item.duration}</td>
+              <td>{item.location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
