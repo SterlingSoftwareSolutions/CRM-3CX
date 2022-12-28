2d91dba923d8df3e057ad7902a1a680a46410a71
Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
2022-12-28 18:20:47 +0530

    Revert "Feature/dashboard changes (#29)"
    This reverts commit b9c46b063fa5eaa10088a1def3c04ec6954f5e3d.

--- frontend/src/Components/SideNavBar/SideNavBar.js
+++ frontend/src/Components/SideNavBar/SideNavBar.js
@@ -3,11 +3,32 @@ import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import "./SideNavBar.css";
-import { Dashboard } from "@mui/icons-material";
 
 const SideNavBar = () => {
   const [isExpanded, setExpandedState] = useState(false);
-  
+  const menuItems = [
+    {
+      text: "Dashboard",
+      icon: "icon/grid.svg",
+      href: "/",
+    },
+    {
+      text: "Inquire",
+      icon: "icon/grid.svg",
+      href: "/inquire",
+    },
+    {
+      text: "Users",
+      icon: "icon/grid.svg",
+      href: "/users",
+    },
+    {
+      text: "Agents",
+      icon: "icon/grid.svg",
+      href: "/agents",
+    },
+  ];
+
   return (
     <div
       className={
@@ -35,58 +56,13 @@ const SideNavBar = () => {
             <span></span>
           </button>
         </div>
-        <div className="nav-menu"></div>
         <div className="nav-menu">
-          <img
-            src={require("./icons/dashboard.png")}
-            className="nav-menu-img"
-            alt="img"
-          />
-          <a
-            href={"/"}
-            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
-          >
-            {isExpanded && <p>{"Dashboard"}</p>}
-          </a>
-        </div>
-        <div className="nav-menu">
-          <img
-            src={require("./icons//inquiry.png")}
-            className="nav-menu-img"
-            alt="img"
-          />
-          <a
-            href={"/inquire"}
-            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
-          >
-            {isExpanded && <p>{"Inquire"}</p>}
-          </a>
-        </div>
-        <div className="nav-menu">
-          <img
-            src={require("./icons/users.png")}
-            className="nav-menu-img"
-            alt="img"
-          />
-          <a
-            href={"/users"}
-            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
-          >
-            {isExpanded && <p>{"Users"}</p>}
-          </a>
-        </div>
-        <div className="nav-menu">
-          <img
-            src={require("./icons/agent.png")}
-            className="nav-menu-img"
-            alt="img"
-          />
-          <a
-            href={"/agents"}
-            className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
-          >
-            {isExpanded && <p>{"Agents"}</p>}
-          </a>
+          {menuItems.map(({text, icon ,href}) => (
+            <a href={href} className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}>
+              <img src={icon} alt="img" srcSet="" />
+              {isExpanded && <p>{text}</p>}
+            </a>
+          ))}
         </div>
       </div>
     </div>
