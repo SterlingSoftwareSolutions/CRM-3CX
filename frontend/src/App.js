b9c46b063fa5eaa10088a1def3c04ec6954f5e3d
Malisha Ponweera <62000985+Malish96@users.noreply.github.com>
2022-12-28 18:18:24 +0530

    Feature/dashboard changes (#29)
    * Database Connect with the CallTypes Popup page
    
    * update
    
    * updates
    
    * side bar design done
    
    * dashboard changes
    
    * side bar design done (#16)
    
    Co-authored-by: Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
    
    * side menu done
    
    * Routing Fixed
    
    * Feature/sidebar (#17)
    
    * side bar design done
    
    * side menu done
    
    * Routing Fixed
    
    Co-authored-by: Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
    
    * dashboard orientation
    
    * Dashboard changes (#18)
    
    * Feature/sidebar (#17)
    
    * side bar design done
    
    * side menu done
    
    * Routing Fixed
    
    Co-authored-by: Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
    
    * dashboard orientation
    
    Co-authored-by: Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
    Co-authored-by: Malish96 <malisha.ponweera@towcha.com.au>
    
    * removed Icons folder from the public folder and moved to src
    
    * update
    
    * Feature/inquiry popupp (#20)
    
    * inquiry page
    
    * remove tag
    
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
    
    * Init
    
    * Dashboard changes are donw
    
    * complete
    
    * dashboard changes
    
    * charts added
    
    Co-authored-by: Sajith3992 <k3992.sajith@gmail.com>
    Co-authored-by: Ravindu_Dissanayaka <ravindu.dissanayaka@towcha.com.au>
    Co-authored-by: Malish96 <malisha.ponweera@towcha.com.au>
    Co-authored-by: Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
    Co-authored-by: Ravindu51099 <104343311+Ravindu51099@users.noreply.github.com>
    Co-authored-by: Sajith3992 <113223741+Sajith3992@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import CustomerPopUp from "./Components/PopCustomer/CustomerPopUp";
 import Dashboard from "./Pages/Dashboard/index";
 import SideNavBar from "./Components/SideNavBar/SideNavBar";
-import Types from "./Components/Inquiries/Types";
 import { Col, Row } from "antd";
 
 function App() {
@@ -20,8 +19,6 @@ function App() {
             <Route path="agents" element={<CustomerPopUp />} />
             <Route path="contact" element={<Dashboard />} />
             <Route path="*" element={<Dashboard />} />
-            <Route path="/customer/:phone" element={<CustomerPopUp />} />
-            <Route path="/types" element={<Types />} />
           </Routes>
         </Col>
       </BrowserRouter>
