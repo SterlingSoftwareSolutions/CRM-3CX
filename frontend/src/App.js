acb309d83c9031df7bb0b11c96b231208c4dce4b
Maneth Pathirana <61646614+SmpManeth@users.noreply.github.com>
2022-12-30 14:52:27 +0530

    404 page Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Login from "./Pages/LoginPage/index";
 import { Col, Row } from "antd";
 import Inquiry from "./Components/Inquiries/Inquiry";
 import AllInquiry from "./Components/Inquiries/all";
+import Errorpage from "./Pages/404/404";
 
 function App() {
   const loggedInUser = sessionStorage.getItem("authenticated");
@@ -17,25 +18,25 @@ function App() {
   );
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+        <Row>
+          <Col span={4}>{authenticated ? <SideNavBar /> : ""}</Col>
+          <Col span={20}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/customer/:phone" element={<CustomerPopUp />} />
+              <Route path="/types" element={<Types />} />
+              <Route path="/inquiry-popup" element={<Inquiry />} />
+              <Route path="/inquiries" element={<AllInquiry />} />
+              <Route path="/*" element={<Errorpage />} />
+            </Routes>
+          </Col>
+        </Row>
+      </BrowserRouter>
     
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/login" element={<Login />} />
-          </Routes>
-          <Row>
-            <Col span={4}>{authenticated ? <SideNavBar /> : ""}</Col>
-            <Col span={20}>
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/customer/:phone" element={<CustomerPopUp />} />
-                <Route path="/types" element={<Types />} />
-                <Route path="/inquiry-popup" element={<Inquiry />} />
-                <Route path="/inquiries" element={<AllInquiry />} />
-              </Routes>
-            </Col>
-          </Row>
-        </BrowserRouter>
-      
     </div>
   );
 }
